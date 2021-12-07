99bc096614836c0d2d7e52ec072e4218683182bd
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2021-12-08 02:02:59 +0530

    convert into small child widgets

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -7,88 +7,106 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // provides height and width of device
     Size size = MediaQuery.of(context).size;
 
-    return Column(
-      children: [
-        Container(
-          height: size.height * 0.2,
-          // color: Colors.black,
-          child: Stack(
-            children: [
-              Container(
-                padding: const EdgeInsets.only(
-                  left: kDefaultPadding,
-                  right: kDefaultPadding,
-                  bottom: 35 + kDefaultPadding,
-                ),
-                height: size.height * 0.2 - 27,
-                decoration: const BoxDecoration(
-                  color: kPrimaryColor,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(36),
-                    bottomRight: Radius.circular(36),
-                  ),
-                ),
-                child: Row(
-                  children: [
-                    Text(
-                      "Hi Abhi!",
-                      style: Theme.of(context).textTheme.headline5!.copyWith(
-                          color: Colors.white, fontWeight: FontWeight.bold),
-                    ),
-                    const Spacer(),
-                    Image.asset("assets/images/logo.png")
-                  ],
-                ),
+    // it enables scrolling on small device
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          HeaderWithSearch(size: size),
+        ],
+      ),
+    );
+  }
+}
+
+class HeaderWithSearch extends StatelessWidget {
+  const HeaderWithSearch({
+    Key? key,
+    required this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: kDefaultPadding),
+      // 20% of total height
+      height: size.height * 0.2,
+      // color: Colors.black,
+      child: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(
+              left: kDefaultPadding,
+              right: kDefaultPadding,
+              bottom: 35 + kDefaultPadding,
+            ),
+            height: size.height * 0.2 - 27,
+            decoration: const BoxDecoration(
+              color: kPrimaryColor,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(36),
+                bottomRight: Radius.circular(36),
               ),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  alignment: Alignment.center,
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  height: 54,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                          offset: const Offset(0, 10),
-                          blurRadius: 50,
-                          color: kPrimaryColor.withOpacity(0.23))
-                    ],
-                  ),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          onChanged: (value) {},
-                          decoration: InputDecoration(
-                            hintText: "Search",
-                            hintStyle: TextStyle(
-                              color: kPrimaryColor.withOpacity(0.5),
-                            ),
-                            enabledBorder: InputBorder.none,
-                            focusedBorder: InputBorder.none,
-                            // suffixIcon:
-                            //     SvgPicture.asset("assets/icons/search.svg"),
-                          ),
-                        ),
-                      ),
-                      SvgPicture.asset("assets/icons/search.svg"),
-                    ],
-                  ),
+            ),
+            child: Row(
+              children: [
+                Text(
+                  "Hi Abhi!",
+                  style: Theme.of(context).textTheme.headline5!.copyWith(
+                      color: Colors.white, fontWeight: FontWeight.bold),
                 ),
-              )
-            ],
+                const Spacer(),
+                Image.asset("assets/images/logo.png")
+              ],
+            ),
           ),
-        )
-      ],
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              height: 54,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                      offset: const Offset(0, 10),
+                      blurRadius: 50,
+                      color: kPrimaryColor.withOpacity(0.23))
+                ],
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      onChanged: (value) {},
+                      decoration: InputDecoration(
+                        hintText: "Search",
+                        hintStyle: TextStyle(
+                          color: kPrimaryColor.withOpacity(0.5),
+                        ),
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        // suffixIcon:
+                        //     SvgPicture.asset("assets/icons/search.svg"),
+                      ),
+                    ),
+                  ),
+                  SvgPicture.asset("assets/icons/search.svg"),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
