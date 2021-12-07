8379a7eaf4fce8b7e7c6135201052dec4e1e2ba2
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2021-12-08 01:56:12 +0530

    header with search

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -17,6 +17,11 @@ class Body extends StatelessWidget {
           child: Stack(
             children: [
               Container(
+                padding: const EdgeInsets.only(
+                  left: kDefaultPadding,
+                  right: kDefaultPadding,
+                  bottom: 35 + kDefaultPadding,
+                ),
                 height: size.height * 0.2 - 27,
                 decoration: const BoxDecoration(
                   color: kPrimaryColor,
@@ -24,6 +29,17 @@ class Body extends StatelessWidget {
                     bottomLeft: Radius.circular(36),
                     bottomRight: Radius.circular(36),
                   ),
+                ),
+                child: Row(
+                  children: [
+                    Text(
+                      "Hi Abhi!",
+                      style: Theme.of(context).textTheme.headline5!.copyWith(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                    const Spacer(),
+                    Image.asset("assets/images/logo.png")
+                  ],
                 ),
               ),
               Positioned(
