ca0e19b8284957fe35ee62b411abcbd86e77051c
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2021-12-09 01:00:52 +0530

    move plat list widget into seprate file

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plant_app/constans.dart';
 
 import 'header_with_search.dart';
+import 'recomend_plats.dart';
 import 'title_with_more_btn.dart';
 
 class Body extends StatelessWidget {
@@ -19,111 +20,7 @@ class Body extends StatelessWidget {
         children: [
           HeaderWithSearch(size: size),
           TitleWiteMoreBtn(press: () {}, title: 'Recomended'),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                RecomendPlantCard(
-                    image: "assets/images/image_1.png",
-                    title: "samantha",
-                    country: "russia",
-                    price: 440,
-                    press: () {}),
-                RecomendPlantCard(
-                    image: "assets/images/image_1.png",
-                    title: "samantha",
-                    country: "russia",
-                    price: 440,
-                    press: () {}),
-                RecomendPlantCard(
-                    image: "assets/images/image_1.png",
-                    title: "samantha",
-                    country: "russia",
-                    price: 440,
-                    press: () {})
-              ],
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class RecomendPlantCard extends StatelessWidget {
-  const RecomendPlantCard({
-    Key? key,
-    required this.image,
-    required this.title,
-    required this.country,
-    required this.price,
-    required this.press,
-  }) : super(key: key);
-
-  final String image, title, country;
-  final int price;
-  final Function press;
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      width: size.width * 0.4,
-      margin: const EdgeInsets.only(
-        left: kDefaultPadding,
-        top: kDefaultPadding / 2,
-        bottom: kDefaultPadding * 2.5,
-      ),
-      child: Column(
-        children: [
-          Image.asset(image),
-          GestureDetector(
-            // onTap: press,
-            child: Container(
-              padding: const EdgeInsets.all(kDefaultPadding / 2),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: const BorderRadius.only(
-                  bottomLeft: Radius.circular(10),
-                  bottomRight: Radius.circular(10),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                      offset: const Offset(0, 10),
-                      blurRadius: 50,
-                      color: kPrimaryColor.withOpacity(0.23)),
-                ],
-              ),
-              child: Row(
-                children: [
-                  RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                          text: "$title\n".toUpperCase(),
-                          style: Theme.of(context).textTheme.button,
-                        ),
-                        TextSpan(
-                          text: "$country".toUpperCase(),
-                          style: TextStyle(
-                            color: kPrimaryColor.withOpacity(0.5),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Spacer(),
-                  Text(
-                    "\$$price",
-                    style: Theme.of(context)
-                        .textTheme
-                        .button!
-                        .copyWith(color: kPrimaryColor),
-                  )
-                ],
-              ),
-            ),
-          )
+          RecomendsPlants()
         ],
       ),
     );
