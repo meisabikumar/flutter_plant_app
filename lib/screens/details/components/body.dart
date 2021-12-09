66326325248dbbf31d89f495a0fed96949fe2786
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2021-12-09 16:12:51 +0530

    bottom buttons

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -15,10 +15,48 @@ class DetailBody extends StatelessWidget {
       child: Column(
         children: [
           ImageAndIcons(size: size),
-          TitleAndPrice(
-            country: 'Russia',
-            price: 400,
-            title: 'Angelica',
+          TitleAndPrice(country: 'Russia', price: 400, title: 'Angelica'),
+          SizedBox(
+            height: kDefaultPadding,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: size.width / 2,
+                height: 84,
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text("Buy Now"),
+                  style: TextButton.styleFrom(
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                      ),
+                    ),
+                    primary: Colors.white,
+                    backgroundColor: kPrimaryColor,
+                    textStyle: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text("Buy Now"),
+                  style: TextButton.styleFrom(
+                    primary: Colors.black,
+                    // backgroundColor: kPrimaryColor,
+                    textStyle: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              )
+            ],
           ),
         ],
       ),
