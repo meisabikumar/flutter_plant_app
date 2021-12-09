00bc937a5fa278a40311ad9054f719abbd2acd50
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2021-12-09 15:38:02 +0530

    icon_card to file

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plant_app/constans.dart';
+import 'package:plant_app/screens/details/components/icon_card.dart';
 
 class DetailBody extends StatelessWidget {
   const DetailBody({Key? key}) : super(key: key);
@@ -70,47 +71,6 @@ class DetailBody extends StatelessWidget {
           ),
         )
       ],
-    );
-  }
-}
-
-class IconCard extends StatelessWidget {
-  const IconCard({
-    Key? key,
-    required this.icon,
-  }) : super(key: key);
-
-  final String icon;
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      margin: EdgeInsets.symmetric(
-        vertical: size.height * 0.03,
-      ),
-      padding: const EdgeInsets.all(
-        kDefaultPadding / 1.5,
-      ),
-      height: 62,
-      width: 62,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(6),
-        boxShadow: [
-          BoxShadow(
-            offset: const Offset(0, 15),
-            blurRadius: 22,
-            color: kPrimaryColor.withOpacity(0.22),
-          ),
-          const BoxShadow(
-            offset: Offset(-15, -15),
-            blurRadius: 20,
-            color: Colors.white,
-          ),
-        ],
-      ),
-      child: SvgPicture.asset(icon),
     );
   }
 }
